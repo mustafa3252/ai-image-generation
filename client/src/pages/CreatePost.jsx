c079d55c9e15473d54befc5db06c946030411b0a
Mustafa Africawala <m8460142878@gmail.com>
2023-03-31 16:28:00 +0530

    made ui changes to CreatePost

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -6,8 +6,32 @@ import {getRandomPrompt} from '../utils';
 import {FormFeild, Loader} from '../components'; 
 
 const CreatePost = () => {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+  });
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {}
+  
   return (
-    <div>CreatePost</div>
+    <section className="max-w-7xl mx-auto">
+
+    <div>
+    <h1 className="font-extrabold text-[#222328] text-32">
+          Create
+        </h1>
+
+        <p className="mt-2 text-[#66  6e75] text-[16px] max-w[500px]">Create imaginative and visually stunning images through DALL-E AI and share them with the community</p>
+    </div>
+
+    <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+    
+    </form>
+
+    </section>
   )
 }
 
