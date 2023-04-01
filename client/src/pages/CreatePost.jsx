cb1282ad80175838954ffb4404d3864d6ebddc87
Mustafa Africawala <m8460142878@gmail.com>
2023-04-02 02:36:05 +0530

    crated the ui for FormFeild

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -14,7 +14,11 @@ const CreatePost = () => {
   const [generatingImg, setGeneratingImg] = useState(false);
   const [loading, setLoading] = useState(false);
 
-  const handleSubmit = (e) => {}
+  const handleSubmit = () => {}
+
+  const handleChange = (e) => {}
+
+  const handleSurpriseMe = () => {}
   
   return (
     <section className="max-w-7xl mx-auto">
@@ -28,9 +32,18 @@ const CreatePost = () => {
     </div>
 
     <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
-    
-    </form>
+      <div className="flex flex-col gap-5">
 
+    <FormFeild labelName="Your name" type="text" name="name" placeholder="John Doe" value={form.name} handleChange={handleChange}/>
+
+    <FormFeild labelName="Promt" type="text" name="prompt" placeholder="An astronaut lounging in a tropical resort in space, vaporwave" value={form.prompt} handleChange={handleChange} isSurpriseMe handleSurprise={handleSurpriseMe}/>
+
+    <div className="relative bg-gray-50 border border-grey-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+
+      {form.photo ? (<img scr={form.photo} alt={form.prompt} className="w-full h-full object-contain"/>):(<img src={preview} alt="preview" className="w-9/12 h-9/12 object-contain opacity-40"/>)}
+        </div>
+        </div>
+      </form>
     </section>
   )
 }
